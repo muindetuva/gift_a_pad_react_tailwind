9801403845a995ea632c3014c254a3c69a9f690e
Alfred Tuva <muindetuva@gmail.com>
2024-12-10 20:35:48 +0300

    Add update

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,6 +5,8 @@ function HomePage() {
     <>
       <Navbar></Navbar>
       <h1>Welcome to the Home Page</h1>
+
+      <p>This is an update we added at 2035hrs on Tuesday 10th December</p>
     </>
   );
 }
